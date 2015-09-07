a05c889271c5fef5bf10378b21bb2b9322897326
Administrator <a@a.aa>
2015-09-07 18:14:23 +0400

    FieldTest - created

---
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -0,0 +1,31 @@
+package io.hexlet.xo.model;
+
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 9/7/2015.
+ */
+public class FieldTest {
+
+    @Test
+    public void testGetSize() throws Exception {
+        final Field field = new Field();
+        assertEquals(3, field.getSize());
+    }
+
+    @Test
+    public void testSetFigure() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, 0);
+        final Figure inputFigure = Figure.O;
+
+        field.setFigure(inputPoint, inputFigure);
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertEquals(inputFigure, actualFigure);
+    }
+}
